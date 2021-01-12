185ff75932cc544918b8bc0556df084e914a61ba
Vladimir Logachev <logachev.dev@ya.ru>
2021-01-12 13:26:57 +0700

    Slightly refactor codegen module

--- backend/src/Codegen.hs
+++ backend/src/Codegen.hs
@@ -8,6 +8,7 @@ module Codegen where
 
 import Data.Foldable
 import qualified Data.HashMap.Lazy as HashMap
+import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Text.Prettyprint.Doc (Doc)
 import Data.Text.Prettyprint.Doc.Render.Text (hPutDoc)
@@ -22,9 +23,12 @@ import System.Directory (createDirectoryIfMissing)
 import System.FilePath.Posix (takeDirectory)
 import System.IO (IOMode (..), withFile)
 
+elmSrcPath :: Text
+elmSrcPath = "../frontend/src/"
+
 writeContentsToFile :: forall ann. Module -> Doc ann -> IO ()
 writeContentsToFile moduleName contents = do
-  let path = T.unpack $ "../frontend/src/" <> T.intercalate "/" moduleName <> ".elm"
+  let path = T.unpack $ elmSrcPath <> T.intercalate "/" moduleName <> ".elm"
   createDirectoryIfMissing True $ takeDirectory path
   putStrLn $ "Writing file: " <> path
   withFile path WriteMode (`hPutDoc` contents)
