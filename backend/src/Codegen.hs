023bf329247233e35193b8bd345e96ca8adc649d
Vladimir Logachev <logachev.dev@ya.ru>
2021-01-19 12:53:40 +0700

    Implement obsolete code deletion

--- backend/src/Codegen.hs
+++ backend/src/Codegen.hs
@@ -6,6 +6,7 @@
 -- This module is responsible for generating Elm files, but not for defining types
 module Codegen where
 
+import Control.Monad (when)
 import Data.Foldable
 import qualified Data.HashMap.Lazy as HashMap
 import Data.Text (Text)
@@ -19,16 +20,17 @@ import qualified Language.Elm.Pretty as Pretty
 import qualified Language.Elm.Simplification as Simplification
 import Servant.To.Elm
 import Server
-import System.Directory (createDirectoryIfMissing)
+import System.Directory (createDirectoryIfMissing, doesDirectoryExist, removeDirectoryRecursive)
 import System.FilePath.Posix (takeDirectory)
 import System.IO (IOMode (..), withFile)
 
-elmSrcPath :: Text
+elmSrcPath :: FilePath
 elmSrcPath = "../frontend/src/"
 
 writeContentsToFile :: forall ann. Module -> Doc ann -> IO ()
 writeContentsToFile moduleName contents = do
-  let path = T.unpack $ elmSrcPath <> T.intercalate "/" moduleName <> ".elm"
+  let path = elmSrcPath <> T.unpack (T.intercalate "/" moduleName) <> ".elm"
+  -- It's important to check and create missing directories individually, because there may several directories
   createDirectoryIfMissing True $ takeDirectory path
   putStrLn $ "Writing file: " <> path
   withFile path WriteMode (`hPutDoc` contents)
@@ -36,8 +38,21 @@ writeContentsToFile moduleName contents = do
 endpointDefinitions :: [Definition]
 endpointDefinitions = map (elmEndpointDefinition "Config.urlBase" ["Api", "Api"]) (elmEndpoints @LibraryAPI)
 
+recursivelyDeleteDirectories :: [Text] -> IO ()
+recursivelyDeleteDirectories subdirectories = do
+  let directories = (elmSrcPath <>) . T.unpack <$> subdirectories
+  forM_ directories $ \d -> do
+    doesExist <- doesDirectoryExist d
+    when doesExist $ do
+      putStrLn $ "Recursively deleting a directory: " <> d
+      removeDirectoryRecursive d
+
 runCodegen :: IO ()
 runCodegen = do
+  -- Since there isn't any restriction on writing the generated code to different directories,
+  -- we can't say for sure which the previous directories were,
+  -- therefore we explicitly specify directories we want to be deleted recursively before writing new files.
+  recursivelyDeleteDirectories ["Api", "xxxx"]
   putStrLn "Generating Elm types and codecs..."
   -- Combine endpoint definitions with type definitions and divide them into modules
   let modules = Pretty.modules $ Simplification.simplifyDefinition <$> (endpointDefinitions <> typeDefinitions)
