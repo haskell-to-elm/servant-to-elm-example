2e43840f2960111dfda356500fca146528dd2f63
Vladimir Logachev <logachev.dev@ya.ru>
2021-03-05 02:40:49 +0700

    Apply elm-format to generated elm code

--- backend/src/Codegen.hs
+++ backend/src/Codegen.hs
@@ -5,11 +5,13 @@
 -- This module is responsible for generating Elm files, but not for defining types
 module Codegen where
 
-import Control.Monad (when)
+import Control.Monad
 import Data.Foldable
 import qualified Data.HashMap.Lazy as HashMap
+import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 import Data.Text.Prettyprint.Doc (Doc)
 import Data.Text.Prettyprint.Doc.Render.Text (hPutDoc)
 import DomainModel
@@ -19,9 +21,11 @@ import qualified Language.Elm.Pretty as Pretty
 import qualified Language.Elm.Simplification as Simplification
 import Servant.To.Elm
 import Server
-import System.Directory (createDirectoryIfMissing, doesDirectoryExist, removeDirectoryRecursive)
+import System.Directory (createDirectoryIfMissing, doesDirectoryExist, findExecutable, removeDirectoryRecursive)
+import System.Exit
 import System.FilePath.Posix (takeDirectory)
 import System.IO (IOMode (..), withFile)
+import System.Process (readProcessWithExitCode)
 
 elmSrcPath :: FilePath
 elmSrcPath = "../frontend/src/"
@@ -43,17 +47,40 @@ recursivelyDeleteDirectories subdirectories = do
   forM_ directories $ \d -> do
     doesExist <- doesDirectoryExist d
     when doesExist $ do
+      putStrLn ""
       putStrLn $ "Recursively deleting a directory: " <> d
       removeDirectoryRecursive d
+
+formatGeneratedCode :: [Text] -> IO ExitCode
+formatGeneratedCode directories = do
+  putStrLn ""
+  mElmFormat <- findExecutable "elm-format"
+  case mElmFormat of
+    Just elmFormatPath -> do
+      putStrLn $ "Found elm-format: " <> elmFormatPath
+      putStrLn ""
+      codes <- forM directories $ \dirName -> do
+        let dir = elmSrcPath <> T.unpack dirName
+        putStrLn $ "Formatting .elm files in a directory (recursively): " <> dir
+        (code, stdout, stderr) <- readProcessWithExitCode elmFormatPath [dir, "--yes"] ""
+        unless (null stdout) $ putStrLn stdout
+        unless (null stderr) $ putStrLn stderr
+        return code
+      return . fromMaybe ExitSuccess $ find (/= ExitSuccess) codes
+    Nothing -> do
+      TIO.putStrLn "elm-format was not found in PATH, leaving output unformatted."
+      return ExitSuccess
 
 runCodegen :: IO ()
 runCodegen = do
   -- Since there isn't any restriction on writing the generated code to different directories,
   -- we can't say for sure which the previous directories were,
-  -- therefore we explicitly specify directories we want to be deleted recursively before writing new files.
-  recursivelyDeleteDirectories ["Api", "xxxx"]
-  putStrLn "Generating Elm types and codecs..."
+  -- therefore we explicitly specify a list of directories we want to be deleted recursively before writing new files.
+  recursivelyDeleteDirectories ["Api"]
+  TIO.putStrLn ""
+  TIO.putStrLn "Generating Elm types and codecs..."
   -- Combine endpoint definitions with type definitions and divide them into modules
   let modules = Pretty.modules $ Simplification.simplifyDefinition <$> (endpointDefinitions <> typeDefinitions)
   -- For each module write contents to file
   forM_ (HashMap.toList modules) $ uncurry writeContentsToFile
+  formatGeneratedCode ["Api"] >>= exitWith
