cccd190046b1660ed3b4592f2a3acd8f627fa3b3
Vladimir Logachev <vladimir@logachev.dev>
2023-05-03 18:33:24 +0400

    Upd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,19 @@ import elmPlugin from "vite-plugin-elm";
 export default defineConfig(({ command }) => {
   if (command === "build") {
     // Production settings
-    return { plugins: [elmPlugin({ debug: false, optimize: true })] };
+    return {
+      plugins: [elmPlugin({ debug: false, optimize: true })],
+      server: {
+        port: 8080,
+      },
+    };
   } else {
     // Dev settings
-    return { plugins: [elmPlugin({ debug: false, optimize: false })] };
+    return {
+      plugins: [elmPlugin({ debug: false, optimize: false })],
+      server: {
+        port: 8080,
+      },
+    };
   }
 });
