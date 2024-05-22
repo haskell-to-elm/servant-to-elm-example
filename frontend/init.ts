31a731ae3e5f15220fa294185016c8f75c424982
Vladimir Logachev <vladimir@logachev.dev>
2024-05-22 04:13:14 +0400

    Update vite

--- frontend/init.ts
+++ frontend/init.ts
@@ -1,8 +1,9 @@
+// @ts-ignore
 import { Elm } from "./src/Main.elm";
 
+// @ts-ignore
 if (process.env.NODE_ENV === "development") {
   const ElmDebugTransform = await import("elm-debug-transformer");
-
   ElmDebugTransform.register({
     simple_mode: true,
   });
---
+++ frontend/vite.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "vite";
+// @ts-ignore
+import { resolve } from "path";
+import elmPlugin from "vite-plugin-elm";
+
+// --------- User-adjustable
+
+const host = "0.0.0.0"; // "0.0.0.0" = Allow clients from local network
+
+const elmDevSettings = {
+  debug: false, // true = enable Elm debugger and show debugger UI
+  optimize: false,
+  nodeElmCompilerOptions: {},
+};
+
+// --------- App-specific
+
+const port = 8080;
+
+// --------- Common
+
+export default defineConfig(({ command }) => {
+  const elmProdSettings = {
+    debug: false,
+    optimize: true,
+    nodeElmCompilerOptions: {},
+  };
+  const elmSettings = command === "build" ? elmProdSettings : elmDevSettings;
+
+  return {
+    plugins: [elmPlugin(elmSettings)],
+    server: { host, port, strictPort: true },
+    build: {
+      rollupOptions: {
+        input: {
+          // @ts-ignore
+          main: resolve(__dirname, "index.html"),
+          // @ts-ignore
+          error: resolve(__dirname, "error.html"),
+        },
+      },
+    },
+    preview: { host, port, strictPort: true },
+  };
+});
